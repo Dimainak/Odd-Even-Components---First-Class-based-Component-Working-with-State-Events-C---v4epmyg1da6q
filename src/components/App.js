0f2313c80a7bb637ec6185767a363cd11e29b389
Mainak Mishra <111529224+Dimainak@users.noreply.github.com>
2023-01-10 20:48:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import '../styles/App.css';
 
 class Odd extends React.Component {
 
+  componentWillUnmount() {
+    console.log('Odd is unmounted');
+  }
   render() {
     return (
       <div id="odd">
@@ -13,6 +16,11 @@ class Odd extends React.Component {
 }
 
 class Even extends React.Component {
+
+  componentWillUnmount() {
+    console.log('Even is unmounted');
+  }
+
 
   render() {
     return (
